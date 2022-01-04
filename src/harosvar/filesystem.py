8b36bb7b17673432979efe63aaf0ec717693aa35
git-afsantos <contact.andre.santos@gmail.com>
2022-01-04 08:54:04 +0000

    changed type annotations

--- src/harosvar/filesystem.py
+++ src/harosvar/filesystem.py
@@ -5,7 +5,7 @@
 # Imports
 ###############################################################################
 
-from typing import Dict, Final, List
+from typing import Dict, Final, Iterable, List, Union
 
 import os
 from pathlib import Path
@@ -16,17 +16,19 @@ from pathlib import Path
 
 EXCLUDED_DIRS: Final = ('doc', 'cmake', '__pycache__')
 
+AnyPath: Final = Union[str, Path]
+
 ###############################################################################
 # Top-level Functions
 ###############################################################################
 
 
-def find_packages(paths: List[str]) -> Dict[str, str]:
+def find_packages(paths: Iterable[AnyPath]) -> Dict[str, str]:
     """
     Find ROS packages inside directories.
 
-    :param paths: [list] of [str] File system path to search.
-    :returns: [dict] Dictionary of [str]package_name -> [str]package_path.
+    :param paths: [Iterable] of file system paths to search.
+    :returns: [dict] Dictionary of [str] package name -> [str] package path.
     """
     ros_version = os.environ.get('ROS_VERSION', '1')
     if ros_version != '1':
@@ -34,26 +36,39 @@ def find_packages(paths: List[str]) -> Dict[str, str]:
     return _find_packages_ros1(paths)
 
 
-def find_launch_xml_files(path: str) -> List[str]:
+def find_launch_xml_files(path: AnyPath) -> List[str]:
     """
     Find ROS Launch XML files, given a (package) path.
 
-    :param path: [str] root directory to search for launch files.
-    :returns: [list] List of [str] paths to launch files.
+    :param path: Root directory to search for launch files.
+    :returns: [list] of [str] paths to launch files.
     """
     filepaths: List[str] = []
-    for root, subdirs, filenames in os.walk(path, topdown=True):
-        current = Path(root).resolve()
+    for root, subdirs, filenames in os.walk(str(path), topdown=True):
+        current: Path = Path(root).resolve()
         if current.name.startswith('.') or current.name in EXCLUDED_DIRS:
             # skip subdirs
             del subdirs[:]
             continue
         for filename in filenames:
-            p = current / filename
+            p: Path = current / filename
             assert p.is_file(), f'not a file: {p}'
             if '.launch' in p.suffixes:
                 # found launch file (e.g., 'a.launch', 'b.launch.xml')
                 filepaths.append(str(p))
+    return filepaths
+
+
+def find_all_launch_xml_files(paths: Iterable[AnyPath]) -> List[str]:
+    """
+    Find ROS Launch XML files, given a list of directories.
+
+    :param paths: [list] of [str] root directories to search for launch files.
+    :returns: [list] of [str] paths to launch files.
+    """
+    filepaths: List[str] = []
+    for path in paths:
+        filepaths.extend(find_launch_xml_files(path))
     return filepaths
 
 
@@ -62,12 +77,12 @@ def find_launch_xml_files(path: str) -> List[str]:
 ###############################################################################
 
 
-def _find_packages_ros1(paths: List[str]) -> Dict[str, str]:
-    # paths: [list] of [str] file system path to search.
-    # returns: [dict] of [str] package -> [str] path.
+def _find_packages_ros1(paths: Iterable[AnyPath]) -> Dict[str, str]:
+    # paths: Iterable of file system paths to search.
+    # returns: [dict] of [str] package name -> [str] package path.
     pkgs: Dict[str, str] = {}
     for path in paths:
-        for root, subdirs, filenames in os.walk(path, topdown=True):
+        for root, subdirs, filenames in os.walk(str(path), topdown=True):
             p = Path(root).resolve()
             name = p.name
             if name.startswith('.') or name in EXCLUDED_DIRS:
@@ -82,7 +97,7 @@ def _find_packages_ros1(paths: List[str]) -> Dict[str, str]:
     return pkgs
 
 
-def _find_packages_ros2(paths: List[str]) -> Dict[str, str]:
-    # paths: [list] of [str] file system path to search.
-    # returns: [dict] of [str] package -> [str] path.
+def _find_packages_ros2(paths: Iterable[AnyPath]) -> Dict[str, str]:
+    # paths: Iterable of file system paths to search.
+    # returns: [dict] of [str] package name -> [str] package path.
     return {}
